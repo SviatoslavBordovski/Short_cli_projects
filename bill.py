dca4e5e59abe421d5ce93d9cdce53109e2ecc6b2
Sviatoslav Bordovski <sbordovski@gmail.com>
2020-01-03 15:42:55 +0200

    Create bill.py

---
+++ bill.py
@@ -0,0 +1,22 @@
+#Runs in the terminal perfectly
+
+hrs = input("Enter Hours:")
+rate = input("Enter Rate:")
+
+try: # if it fails because user entered a word - except would run
+   nh = float(hrs) #number of hours
+   hr = float(rate) #hour rate
+
+except:
+   print('Enter a number, not a word')
+   quit()
+
+bill = nh * hr
+
+if nh <= 40:
+   print(bill)
+elif nh > 40:
+   sr = 40 * hr #standard payment
+   ohr = hr * 1.5 #overtime hours rate
+   pay = sr+(ohr*(nh-40))
+   print(pay)
