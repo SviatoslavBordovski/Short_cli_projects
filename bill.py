660e6e7cb6c9d66d40237eb35161a6108a154d0b
Sviatoslav Bordovski <sbordovski@gmail.com>
2022-05-08 11:33:36 +0300

    Update bill.py

--- bill.py
+++ bill.py
@@ -1,11 +1,11 @@
-#Runs in the terminal perfectly
+# Runs in the terminal perfectly
 
 hrs = input("Enter Hours:")
 rate = input("Enter Rate:")
 
-try: #if it fails because user entered a word - except would run
-   nh = float(hrs) #number of hours
-   hr = float(rate) #hour rate
+try:  # if it fails because user entered a word - except would run
+   nh = float(hrs)  # number of hours
+   hr = float(rate)  # hour rate
 
 except:
    print('Enter a number, not a word')
@@ -16,7 +16,7 @@ bill = nh * hr
 if nh <= 40:
    print(bill)
 elif nh > 40:
-   sr = 40 * hr #standard payment
-   ohr = hr * 1.5 #overtime hours rate
+   sr = 40 * hr  # standard payment
+   ohr = hr * 1.5  # overtime hours rate
    pay = sr+(ohr*(nh-40))
    print(pay)
