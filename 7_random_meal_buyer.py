6b0175da2e6bbd73fcacec65886fa164586d124f
Sviatoslav Bordovski <sbordovski@gmail.com>
2022-05-09 16:52:40 +0300

    Update 7_random_meal_buyer.py

--- 7_random_meal_buyer.py
+++ 7_random_meal_buyer.py
@@ -12,6 +12,6 @@ num_names = len(names_list)
 random_person = random.randint(0, num_names - 1)
 
 #Pick out random person from list of names using the random number.
-person_who_will_pay = names_list[random_person]
+bill_payer = names_list[random_person]
 
-print(person_who_will_pay + " is going to buy the meal today!")
+print(bill_payer + " is going to buy the meal today!")
