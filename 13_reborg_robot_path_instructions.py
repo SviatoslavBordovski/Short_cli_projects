938a436014e0e49e9ba9483246dee560686ff14e
Sviatoslav Bordovski <sbordovski@gmail.com>
2022-05-15 19:51:11 +0300

    Update 13_reborg_robot_path_instructions.py

--- 13_reborg_robot_path_instructions.py
+++ 13_reborg_robot_path_instructions.py
@@ -1,4 +1,4 @@
-# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%204&url=worlds%2Ftutorial_en%2Fhurdle4.json
+# Reborg 'Hardle 4' task => https://bit.ly/3FOATcZ
 
 def turn_right():
     turn_left()
