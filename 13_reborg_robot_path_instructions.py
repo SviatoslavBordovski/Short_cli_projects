59a8cd988b7654c4da3f48557c15dd4f04d579a7
Sviatoslav Bordovski <sbordovski@gmail.com>
2022-05-15 20:11:59 +0300

    Update 13_reborg_robot_path_instructions.py

--- 13_reborg_robot_path_instructions.py
+++ 13_reborg_robot_path_instructions.py
@@ -1,5 +1,6 @@
 # Reborg 'Hardle 4' task => https://bit.ly/3FOATcZ
-# Solution is below:
+
+# 1st solution:
 
 def turn_right():
     turn_left()
@@ -30,5 +31,34 @@ def find_a_flag():
             jump()
         elif wall_in_front() or front_is_clear() and right_is_clear():
             turn_right()
+
+find_a_flag()
+
+# /------------------------------------------------------------------------/
+
+# 2nd solution:
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+def jump():
+    turn_left()
+    while wall_on_right():
+        move()
+    turn_right()
+    move()
+    turn_right()
+    while front_is_clear():
+        move()
+    turn_left()
+
+def find_a_flag():
+    while not at_goal():
+        if wall_in_front():
+            jump()
+        else:
+            move()
 
 find_a_flag()
