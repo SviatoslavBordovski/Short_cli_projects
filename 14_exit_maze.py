47a196132c77a1671f22e39790fd9c67e605a87a
Sviatoslav Bordovski <sbordovski@gmail.com>
2022-05-17 10:35:13 +0300

    Update 14_exit_maze.py

--- 14_exit_maze.py
+++ 14_exit_maze.py
@@ -11,9 +11,18 @@ def exit_maze():
             move()
             if right_is_clear():
                 turn_right()
+            else:
+                turn_left()
+                
+             
         elif wall_in_front() and wall_on_right():
             turn_left()
         else:
             turn_right()
-            
+            move()
+
 exit_maze()
+
+
+# 2nd solution:
+
