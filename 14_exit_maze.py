be1120fc3d5b2fed2bebbf8e8a27d16fb0a92439
Sviatoslav Bordovski <sbordovski@gmail.com>
2022-05-17 10:54:59 +0300

    Update 14_exit_maze.py

--- 14_exit_maze.py
+++ 14_exit_maze.py
@@ -26,3 +26,20 @@ exit_maze()
 
 # 2nd solution:
 
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def find_a_flag():
+    while not at_goal():
+        if right_is_clear() and not front_is_clear():
+            turn_right()
+            move()
+        
+        elif front_is_clear():
+            move()
+        else:
+            turn_left()
+
+find_a_flag()
